41c708d386b3c1cd46e41c96dcb36eba52882fff
Anmol Panchal <anmolpanchal1702@gmail.com>
2026-08-13 22:54:08 +0530

    Make API URL configurable for deployment

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -5,8 +5,12 @@ import requests
 # =================================
 # CONFIGURATION
 # =================================
+import os
 
-API_BASE_URL = "http://127.0.0.1:8000"
+API_BASE_URL = os.getenv(
+    "API_BASE_URL",
+    "http://127.0.0.1:8000"
+)
 
 
 st.set_page_config(
